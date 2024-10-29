c0ec8c0c5dd19537e6df08d794a13bb468055aae
Sw1tlana <127736251+Sw1tlana@users.noreply.github.com>
2024-10-29 23:10:04 +0200

    changes .env firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -9,7 +9,7 @@ const firebaseConfig = {
     storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
     messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
     appId: import.meta.env.VITE_APP_ID,
-    databaseURL: `https://${import.meta.env.VITE_PROJECT_ID}.firebaseio.com`
+    databaseURL: import.meta.env.VITE_DATABASE_URL
 };
 
 const app = initializeApp(firebaseConfig);
